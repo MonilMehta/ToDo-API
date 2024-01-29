fb8cf523601da2c0a448a4fa77726d2f1e617b62
MonilMehta <monilmehta5@gmail.com>
2024-01-29 21:31:01 +0530

    Added Profile Model

--- TodoAPI/models.py
+++ TodoAPI/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class ToDo(models.Model):
@@ -6,3 +7,10 @@ class ToDo(models.Model):
     description = models.TextField(blank=True)
     completed = models.BooleanField(default=False)
     date_created = models.DateField(auto_now_add=True)
+
+class Profile(models.Model):
+    username=models.OneToOneField(User,on_delete=models.CASCADE)
+    email=models.EmailField(unique=True)
+
+    def __str__(self) :
+        return  self.username
